c9130b3ae28af63c5c37de548673bce39acc7851
RMLemos <143395332+RMLemos@users.noreply.github.com>
2024-03-02 15:39:45 +0000

    create models

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 from django.db.models import signals
 from django.template.defaultfilters import slugify
+from utils.images import resize_image
 
 class Base(models.Model):
     created_at = models.DateField('Created at', auto_now_add=True)
@@ -25,6 +27,47 @@ class Category(Base):
     def __str__(self) -> str:
         return self.name
     
+
+def category_pre_save(signal, instance, sender, **kwargs):
+    instance.slug = slugify(instance.name)
+
+signals.pre_save.connect(category_pre_save, sender=Category)
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(
+        upload_to='recipes/covers/', blank=True, default='')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True,
+        default=None,
+    )
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        current_picture_name = str(self.picture.name)
+        super_save = super().save(*args, **kwargs)
+        picture_changed = False
+
+        if self.picture:
+            picture_changed = current_picture_name != self.picture.name
+
+        if picture_changed:
+            resize_image(self.picture, 900, True, 70)
+
+        return super_save
 
 def category_pre_save(signal, instance, sender, **kwargs):
     instance.slug = slugify(instance.name)
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from recipes.models import Category, Recipe
 
 def home(request):
+    categories = Category.objects.order_by('name')
+    recipes = Recipe.objects.order_by('-id')
     return render(request, 'recipes/home.html', context={
-        'name': 'Luiz Otávio',
+        'categories': categories,
+        'recipes': recipes,
     })
 
 
